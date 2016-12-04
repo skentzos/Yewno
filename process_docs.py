f8a3f7020d1bd44f32bf3a7acc9dd8b16af286c4
steveskentzos <skentzos@stanford.edu>
2016-12-04 11:23:12 -0800

    Use sets for speed

--- process_docs.py
+++ process_docs.py
@@ -7,60 +7,55 @@ from string_handler import get_token_list
 
 class similar_word_finder(object):
     def __init__(self, path, group):
-        self.total_posts = 0.0
+        self.total_posts = 0                                                # Post counter is also postID.
         self.posts_with_word = {}
-        self.posts_with_both_words = {}
 
-        for filename in filename_iterator(path, group=group):
+        fi = filename_iterator(path, group=group)
+
+        for filename in fi:
             for post in get_posts_from_xml_file(filename):
-                self.total_posts += 1.0                                     # Track total number of posts seen
+                self.total_posts += 1                                       # Track total number of posts seen.
 
-                tokens = list(set(get_token_list(post)))                    # Remove duplicate tokens within same post
+                tokens = list(set(get_token_list(post)))                    # Remove duplicate tokens within same post.
 
-                for index in range(len(tokens)):
-                    token = tokens.pop(index)                               # Iterate over tokens
-                    self.posts_with_word[token] = self.posts_with_word.get(token, 0.0) + 1.0
-                    self.posts_with_both_words.setdefault(token, {})
+                for token in tokens:                                        # Iterate over tokens.
+                    self.posts_with_word.setdefault(token, set())
+                    self.posts_with_word[token].add(self.total_posts)       # Add postID to set of posts with token.
 
-                    for other_token in tokens:
-                        self.posts_with_both_words[token][other_token] = \
-                            self.posts_with_both_words[token].get(other_token, 0.0) + 1.0
+        self.total_posts = float(self.total_posts)                          # For later non-integer division
 
-                    tokens.insert(index, token)
+        self.counts = {}
+        for key in self.posts_with_word:
+            self.counts[key] = len(self.posts_with_word[key])               # Precompute number of posts with token
 
     def get_similarity_score(self, x, y):
 
         # x, y case
-        pxy = self.posts_with_both_words[x].get(y, 0.0) / self.total_posts
-        px = self.posts_with_word[x] / self.total_posts
-        py = self.posts_with_word[y] / self.total_posts
+        pxy = len(self.posts_with_word[x] & self.posts_with_word[y]) / self.total_posts
+        px = self.counts[x] / self.total_posts
+        py = self.counts[y] / self.total_posts
         pp = self.calculate_score(pxy, px, py)
 
         # x, not y case
-        pxy = (self.posts_with_word[x] - self.posts_with_both_words[x].get(y, 0.0)) / self.total_posts
-        px = self.posts_with_word[x] / self.total_posts
-        py = (self.total_posts - self.posts_with_word[y]) / self.total_posts
+        pxy = len(self.posts_with_word[x] - self.posts_with_word[y]) / self.total_posts
+        px = self.counts[x] / self.total_posts
+        py = (self.total_posts - self.counts[y]) / self.total_posts
         pn = self.calculate_score(pxy, px, py)
 
         # not x, y case
-        pxy = (self.posts_with_word[y] - self.posts_with_both_words[y].get(x, 0.0)) / self.total_posts
-        px = (self.total_posts - self.posts_with_word[x]) / self.total_posts
-        py = self.posts_with_word[y] / self.total_posts
+        pxy = len(self.posts_with_word[y] - self.posts_with_word[x]) / self.total_posts
+        px = (self.total_posts - self.counts[x]) / self.total_posts
+        py = self.counts[y] / self.total_posts
         np = self.calculate_score(pxy, px, py)
 
         # not x, not y case
         pxy = (self.total_posts -
-               self.posts_with_word[x] -
-               self.posts_with_word[y] +
-               self.posts_with_both_words[x].get(y, 0.0)) / self.total_posts
-        px = (self.total_posts - self.posts_with_word[x]) / self.total_posts
-        py = (self.total_posts - self.posts_with_word[y]) / self.total_posts
+               self.counts[x] -
+               self.counts[y] +
+               len(self.posts_with_word[x] & self.posts_with_word[y])) / self.total_posts
+        px = (self.total_posts - self.counts[x]) / self.total_posts
+        py = (self.total_posts - self.counts[y]) / self.total_posts
         nn = self.calculate_score(pxy, px, py)
-
-        print pp
-        print pn
-        print np
-        print nn
 
         return pp + pn + np + nn
 
@@ -73,9 +68,9 @@ if __name__ == "__main__":
 
     swf = similar_word_finder(path="./../blogs", group='30s')
 
-    pprint(swf.total_posts)
-    pprint(swf.posts_with_word['winnipeg'])
-    pprint(swf.posts_with_word['with'])
-    pprint(swf.posts_with_both_words['winnipeg']['with'])
+    #pprint(swf.total_posts)
+    #pprint(swf.posts_with_word['winnipeg'])
+    #pprint(swf.posts_with_word['with'])
+    #pprint(swf.posts_with_both_words['winnipeg']['with'])
 
     pprint(swf.get_similarity_score('winnipeg', 'with'))
